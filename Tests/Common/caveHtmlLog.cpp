44cc0c972047b90b68e457d910618a86962234f0
Udo Lugauer <ulugauer@nvidia.com>
2018-02-01 15:12:26 +0100

    Add HTML logging for the sanit tests

---
+++ Tests/Common/caveHtmlLog.cpp
@@ -0,0 +1,183 @@
+/*
+Copyright (c) <2017> <Udo Lugauer>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+*/
+
+/// @file caveHtmlLog.cpp
+///       Functions for writing logs in HTML style
+
+#include "caveHtmlLog.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <errno.h>
+
+static FILE *logFile = NULL; ///< global log file
+
+/**
+* @brief Print timestamp to log file
+*
+* @param[in]  buffer		must be able to hold 26 bytes
+*
+* @return none
+*/
+void CaveHtmlLog::Timestamp(char *buffer)
+{
+	struct tm *newTime;
+	time_t systemTime;
+
+	// print the time stamp
+	if (time(&systemTime) != -1)
+	{
+		// Convert to local time.
+		newTime = localtime(&systemTime);
+		if (newTime)
+		{
+			strcpy(buffer, asctime(newTime));
+
+			// asctime adds a newline, remove this
+			buffer[strlen(buffer) - 1] = 0;
+		}
+	}
+}
+
+
+/**
+* @brief Initialize HTML based logging
+*
+* @param[in]  noLogFile		if true write to stdout
+* @param[in]  header		header appears in the HTML title bar
+*
+* @return true if successful
+*/
+bool CaveHtmlLog::LogInit(bool noLogFile, const char* filePrefix, const char* header)
+{
+	if (noLogFile)
+	{
+		logFile = stdout;
+	}
+	else
+	{
+		size_t prefixLength = strlen(filePrefix);
+		char* fileName = new char[prefixLength + 26 + 5];
+		//char fileName[prefixLength + 26 + 5]; // prefix + timestamp + posix
+
+		strcpy(fileName, filePrefix);
+		Timestamp(&fileName[strlen(fileName)]);
+		strcat(fileName, ".html");
+		// replace : with -
+		for (unsigned int i = 0; i < strlen(fileName); i++)
+		{
+			if (fileName[i] == ':')
+				fileName[i] = '-';
+		}
+
+		logFile = fopen(fileName, "w");
+		if (!logFile)
+			return false;
+
+		fprintf(logFile,
+			"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n"
+			"    \"http://www.w3.org/TR/html4/loose.dtd\">\n"
+			"<html>\n"
+			"<head>\n");
+		fprintf(logFile, "    <title>%s</title>\n", header);
+		fprintf(logFile, "</head>\n"
+			"<body>\n");
+		fprintf(logFile, "<h1>%s</h1>\n", header);
+
+		delete fileName;
+	}
+
+	return true;
+}
+
+/**
+* @brief Cleanup HTML based logging and write end tag
+*
+*
+* @return none
+*/
+void CaveHtmlLog::LogCleanup()
+{
+	fprintf(logFile,
+		"</body>\n"
+		"</html>\n"
+	);
+
+	if (logFile != stdout)
+		fclose(logFile);
+}
+
+/**
+* @brief Write a messeage to the log file
+*
+* @param[in]  message		message buffer to log
+*
+* @return none
+*/
+void CaveHtmlLog::LogMessage(const char *message, ...)
+{
+	va_list marker;
+
+	va_start(marker, message);
+	vfprintf(logFile, message, marker);
+	va_end(marker);
+
+	fprintf(logFile, "<br>");
+}
+
+///< start a HTML paragraph
+void CaveHtmlLog::LogBeginParagraph()
+{
+	fprintf(logFile, "<p>\n");
+}
+
+///< end a HTML paragraph
+void CaveHtmlLog::LogEndParagraph()
+{
+	fprintf(logFile, "</p>\n");
+}
+
+///< write start table tag
+void CaveHtmlLog::LogBeginTable()
+{
+	fprintf(logFile,
+		"<table border=\"1\" cellpadding=\"10\">\n"
+	);
+}
+
+///< write end table tag
+void CaveHtmlLog::LogEndTable()
+{
+	fprintf(logFile, "</table>\n");
+}
+
+///< write row start tags
+void CaveHtmlLog::LogBeginRow()
+{
+	fprintf(logFile, "    <tr><td>");
+}
+
+///< write row end tags
+void CaveHtmlLog::LogEndRow()
+{
+	fprintf(logFile, "</td></tr>\n");
+}
+
+///< write column start tags
+void CaveHtmlLog::LogNextColumn()
+{
+	fprintf(logFile, "</td><td>");
+}
--- Tests/Sanity/caveSanity.cpp
+++ Tests/Sanity/caveSanity.cpp
@@ -17,6 +17,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include "Render/renderRenderPass.h"
 
 #include "../Common/caveImageUtil.h"
+#include "../Common/caveHtmlLog.h"
 
 #include "Base/caveSanityTestDevice.h"
 #include "Base/caveSanityTestMappedVbo.h"
@@ -36,6 +37,11 @@ int32_t				g_WinWidth;				///< window width
 int32_t				g_WinHeight;			///< window height
 bool				g_WriteImage = false;	///< write output images
 string_type			g_OutDirName;			///< path to write output images
+bool				g_CompareImage = false;	///< check gold images
+string_type			g_GoldDirName;			///< path to gold images
+
+bool				g_WriteLog = true;		///< by default write log file
+CaveHtmlLog			g_LogHMTL;				///< class looging the results in HTML
 
 
 
@@ -87,6 +93,13 @@ executeTest(RenderDevice *device, RenderCommandPool* commandPool, RenderPass* re
 
 	std::cerr << testName << "\n";
 
+	if (g_WriteLog)
+	{
+		g_LogHMTL.LogBeginRow();
+		g_LogHMTL.LogMessage(testName);
+		g_LogHMTL.LogNextColumn();
+	}
+
 	if (curTest->IsSupported(device))
 	{
 		success = curTest->Run(device, commandPool, renderPass, pUserData);
@@ -101,10 +114,12 @@ static void
 printHelpMessage()
 {
 	string_type MsgStr = "NDDPathFinder display point clouds.\n\n";
-	MsgStr += " -h			 - prints this help message\n";
-	MsgStr += " -r			 - path to resource files\n";
-	MsgStr += " -o			 - path to image output (directory must exist)\n";
-	MsgStr += " -winSize x y - Set window size\n";
+	MsgStr += " -h				- prints this help message\n";
+	MsgStr += " -noLogFile		- don't write a log file but output all messages to std out\n";
+	MsgStr += " -r				- path to resource files\n";
+	MsgStr += " -o [outdir]		- path to image output (directory must exist)\n";
+	MsgStr += " -g [comparedir] - directory which contains the gold images\n";
+	MsgStr += " -winSize x y	- Set window size\n";
 
 	std::cerr << MsgStr.c_str();
 }
@@ -132,6 +147,13 @@ bool getComdLineArguments(int argc, char** argv)
 			g_ProjectPath = pArgStr;
 		}
 
+		// check if we should write no log file
+		index = pArgStr.find("-noLogFile");
+		if (index != string_type::npos)
+		{
+			g_WriteLog = false;
+		}
+
 		// check if we should write images
 		index = pArgStr.find("-o");
 		if (index != string_type::npos && argv[theIndex + 1] != NULL)
@@ -139,6 +161,15 @@ bool getComdLineArguments(int argc, char** argv)
 			pArgStr = argv[theIndex + 1];
 			g_OutDirName = pArgStr;
 			g_WriteImage = true;
+		}
+
+		// check if we should compare images
+		index = pArgStr.find("-g");
+		if (index != string_type::npos && argv[theIndex + 1] != NULL)
+		{
+			pArgStr = argv[theIndex + 1];
+			g_GoldDirName = pArgStr;
+			g_CompareImage = true;
 		}
 
 		// get window size
@@ -167,7 +198,6 @@ int main(int argc, char* argv[])
 	g_WinHeight = 480;
 	int32_t testPassed = 0;
 	int32_t testFailed = 0;
-
 
 	// get commaqnd line arguments
 	if (!getComdLineArguments(argc, argv))
@@ -283,6 +313,44 @@ int main(int argc, char* argv[])
 		std::cerr << "Failed to create swap chain framebuffers\n";
 	}
 
+	// setup log file
+	if (g_WriteLog && !g_LogHMTL.LogInit(false, "CaveSanityTest", "CaveBackendTest: backend tests"))
+	{
+		std::cerr << "Can't create log file.\n";
+		exit(1);
+	}
+
+	if (g_WriteLog)
+	{
+		uint32_t major, minor, patch;
+
+		g_LogHMTL.LogBeginParagraph();
+		g_LogHMTL.LogMessage("<b>System Information</b>");
+
+#ifdef _WIN32
+		g_LogHMTL.LogMessage("Windows 64 bit");
+#elif defined(__linux__)
+		g_LogHMTL.LogMessage("Linux 64 bit");
+#endif
+		g_LogHMTL.LogEndParagraph();
+
+		// context information
+		g_LogHMTL.LogBeginParagraph();
+		g_LogHMTL.LogMessage("<b>Device Information</b>");
+		g_LogHMTL.LogMessage("Device Name: <font color=\"#8B4726\">%s</font>", renderDevice->GetDeviceName());
+		renderDevice->GetDriverVersion(major, minor, patch);
+		g_LogHMTL.LogMessage("Driver Version: <font color=\"#8B4726\">%d.%d.%d</font>", major, minor, patch);
+		g_LogHMTL.LogMessage("");
+		g_LogHMTL.LogMessage("Backend: <font color=\"#8B4726\">%s</font>", "Vulkan");
+		renderDevice->GetApiVersion(major, minor, patch);
+		g_LogHMTL.LogMessage("API Version: <font color=\"#8B4726\">%d.%d.%d</font>", major, minor, patch);
+		g_LogHMTL.LogEndParagraph();
+
+		g_LogHMTL.LogBeginParagraph();
+		g_LogHMTL.LogMessage("Tests:");
+		g_LogHMTL.LogBeginTable();
+	}
+
 	// allocate buffer for readback
 	uint32_t readSize = userData.winWidth * userData.winHeight * 4;
 	void* pixelBuffer = renderDevice->GetEngineAllocator()->Allocate(readSize, 4);
@@ -295,21 +363,58 @@ int main(int argc, char* argv[])
 	{
 		bool passed = executeTest(renderDevice, graphicsCommandPool, renderPass, testList[i].m_test, testList[i].m_name, &userData);
 
+		if (pixelBuffer && (g_WriteImage || g_CompareImage))
+			renderDevice->ReadPixels(pixelBuffer);
+
 		// write image 
-		if (g_WriteImage)
+		if (pixelBuffer && g_WriteImage)
 		{
 			string_type file = g_OutDirName;
 			file += testList[i].m_name;
 			file += ".png";
 
-			if (pixelBuffer)
-			{
-				renderDevice->ReadPixels(pixelBuffer);
-				image_t image = { (unsigned char *)pixelBuffer, userData.winWidth, userData.winHeight, userData.winWidth * 4, 4, true };
-
-				WritePng(file.c_str(), &image);
-			}
+			image_t image = { (unsigned char *)pixelBuffer, userData.winWidth, userData.winHeight, userData.winWidth * 4, 4, true };
+			WritePng(file.c_str(), &image);
 		}
+
+		if (g_CompareImage && testList[i].m_test->IsImageCompareSupported(renderDevice))
+		{
+			string_type refFile = g_GoldDirName;
+			refFile += testList[i].m_name;
+			refFile += ".png";
+			image_t refImage;
+
+			if (ReadPng(refFile.c_str(), &refImage))
+			{
+				image_t image = { (unsigned char *)pixelBuffer, userData.winWidth, userData.winHeight, userData.winWidth * 4, 4, true };
+				passed &= CmpImage(&refImage, &image, NULL);
+
+				delete refImage.pixels;
+			}
+			else
+			{
+				std::cerr << "NVRenderTest: gold image: " << refFile.c_str() << " not found\n";
+			}
+
+		}
+
+		if (g_WriteLog && testList[i].m_test->IsSupported(renderDevice))
+		{
+			g_LogHMTL.LogMessage(
+				"    <font color=\"%s\">%s</font>",
+				passed ? "#00FF00" : "#FF0000",
+				passed ? "passed" : "failed");
+			g_LogHMTL.LogEndRow();
+		}
+		else if (g_WriteLog)
+		{
+			g_LogHMTL.LogMessage(
+				"    <font color=\"%s\">%s</font>",
+				"#0000FF",
+				"not supported");
+			g_LogHMTL.LogEndRow();
+		}
+
 
 		if (passed)
 			testPassed++;
@@ -320,9 +425,21 @@ int main(int argc, char* argv[])
 	std::cerr << "Tests passed: " << testPassed << "\n";
 	std::cerr << "Tests failed: " << testFailed << "\n";
 
+	if (g_WriteLog)
+	{
+		g_LogHMTL.LogEndTable();
+		g_LogHMTL.LogEndParagraph();
+	}
+
+
 	do {
 		
 	} while (frontend->HandleWindowMessage());
+
+	if (g_WriteLog)
+	{
+		g_LogHMTL.LogCleanup();
+	}
 
 	// destroy test list
 	destroyTestList();
